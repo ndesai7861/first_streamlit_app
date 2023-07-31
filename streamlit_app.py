cb7030a924baa04c7e6c433cd4679aea8f9103d6
ndesai7861 <109726464+ndesai7861@users.noreply.github.com>
2023-07-31 13:36:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy Diner')
-
 streamlit.header('Breakfast Menu')
-
 streamlit.text('Idli Vada')
 streamlit.text('Masala Dosa')
 streamlit.text('Girmit')
