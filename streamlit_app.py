695da1f15924185520939e66224da571dfa94964
ndesai7861 <109726464+ndesai7861@users.noreply.github.com>
2023-07-31 17:16:14 +0530

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Idli Vada - 50 Rs')
-streamlit.text('Masala Dosa - 60 Rs')
-streamlit.text('Girmit - 40 Rs')
+streamlit.text('🥣 Idli Vada - 50 Rs')
+streamlit.text('🍞 Masala Dosa - 60 Rs')
+streamlit.text('🥗 Girmit - 40 Rs')
