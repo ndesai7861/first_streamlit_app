290a142306abaeb4204844c70801ff1c1cbf2504
ndesai7861 <109726464+ndesai7861@users.noreply.github.com>
2023-07-31 13:36:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Idli Vada')
-streamlit.text('Masala Dosa')
-streamlit.text('Girmit')
+streamlit.text('Idli Vada - 50 Rs')
+streamlit.text('Masala Dosa - 60 Rs')
+streamlit.text('Girmit - 40 Rs')
