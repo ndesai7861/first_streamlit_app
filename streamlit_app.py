b28b63eb32705336654678e879222ce86b37667a
ndesai7861 <109726464+ndesai7861@users.noreply.github.com>
2023-08-02 16:42:54 +0530

    Update streamlit_app.py
    Added a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,13 +11,10 @@ streamlit.text('🍞 Masala Dosa - 60 Rs')
 streamlit.text('🥗 Girmit - 40 Rs')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
 #import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
@@ -27,7 +24,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 
 streamlit.dataframe(fruits_to_show)
-
+# Create the repeatable code block 
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())  
+    return fruityvice_normalized
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -35,9 +36,8 @@ try:
     if not fruit_choice:
         streamlit.error("Please select a fruit to get the information")
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 # Do not run until the issue is fixed
